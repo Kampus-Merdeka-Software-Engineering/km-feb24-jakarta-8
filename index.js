8238f3a47e5a0814e7b1692d9d245be7f4a97144
fachrireza <fachrireza0303@gmail.com>
2024-05-17 18:51:26 +0700

    fixing template,uploading data json,displaying data

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ window.addEventListener("DOMContentLoaded", async () => {
   const totalElement = document.getElementById("total");
   const data = await fetchData();
   const jumlahTransaksi = data.length;
+  totalElement.textContent = "Transactions : " + jumlahTransaksi;
 
-  totalElement.textContent = "Jumlah Transaksi:" + (+jumlahTransaksi + 10);
+  const quantityElement = document.getElementById("quantity");
+  const totalOrderQuantity = data.reduce((sum, item) =>
+    sum + item.Order_Quantity, 0);
+  quantityElement.textContent = "Sold : " + totalOrderQuantity;
+
+  const costElement = document.getElementById("cost");
+  const totalCost = data.reduce((sum, item) => 
+    sum + item.Cost, 0);
+  costElement.textContent = "Cost : $" + totalCost;
+
+  const revenueElement = document.getElementById("revenue");
+  const totalRevenue = data.reduce((sum, item) => 
+    sum + item.Revenue, 0);
+  revenueElement.textContent = "Revenue : $" + totalRevenue;
+
 });
